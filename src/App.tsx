eff93759d0a74b762835038207157f38715fcd69
HONGSANGYOON <jay182@naver.com>
2023-12-17 13:54:13 +0900

    모지?

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
+  const [contentList, setContentList] = useState<string>("");
+
+  const inputTextHandler = (e) => {
+    setContentList(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input></input>
+        <InputBtn>확인하기</InputBtn>
+      </div>
+      {/* 할일부분 */}
+      <div>
+        <h2>오늘 할일</h2>
+        {/* input태그에서 갖고온 사항들 리스트로 처리 */}
+      </div>
+      {/* 마무리한일 부분 */}
+    </>
   );
 }
 
+const InputBtn = styled.button`
+  width: 5vw;
+  height: 2.5vh;
+`;
 export default App;
