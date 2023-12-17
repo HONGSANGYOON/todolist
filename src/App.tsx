1d5bac3a469960905d5060952ede80039c5dc97a
HONGSANGYOON <jay182@naver.com>
2023-12-17 14:52:33 +0900

    Feature: input 으로 받은 값 content로 보여주기 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,34 @@ import styled from "styled-components";
 import "./App.css";
 
 function App() {
-  const [contentList, setContentList] = useState<string>("");
+  const [inputContent, setInputContent] = useState<string>("");
+  const [contentList, setContentList] = useState<string[]>([]);
 
-  const inputTextHandler = (e) => {
-    setContentList(e.target.value);
+  const inputTextHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputContent(e.target.value);
   };
 
+  const onClickHandler = () => {
+    setContentList([...contentList, inputContent]);
+    setInputContent("");
+  };
   return (
     <>
       <div>
-        <input></input>
-        <InputBtn>확인하기</InputBtn>
+        <input onChange={inputTextHandler} value={inputContent}></input>
+        <InputBtn onClick={onClickHandler}>확인하기</InputBtn>
       </div>
-      {/* 할일부분 */}
+
       <div>
         <h2>오늘 할일</h2>
         {/* input태그에서 갖고온 사항들 리스트로 처리 */}
+        {contentList.map((content, index) => (
+          <ContentContainer key={index}>
+            <div>{content}</div>
+            <button>삭제하기</button>
+            <button>완료하기</button>
+          </ContentContainer>
+        ))}
       </div>
       {/* 마무리한일 부분 */}
     </>
@@ -28,5 +40,10 @@ function App() {
 const InputBtn = styled.button`
   width: 5vw;
   height: 2.5vh;
+`;
+
+const ContentContainer = styled.div`
+  width: 10vw;
+  height: 10vh;
 `;
 export default App;
